1fc30e0748f57b87bf8c1a1b948442f246ce5f72
Omnicrafter <chen.jeffrey.a@gmail.com>
2024-03-07 23:20:45 -0600

    completed longestFall and pivotIndex

--- longest-fall.js
+++ longest-fall.js
@@ -1,3 +1,22 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+  let maxFall = 1;
+  let currentFall = 1;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      currentFall++;
+      maxFall = Math.max(maxFall, currentFall);
+    } else {
+      currentFall = 1;
+    }
+  }
+
+  return maxFall;
 }
+
+module.exports = longestFall;
--- pivot-index.js
+++ pivot-index.js
@@ -1,3 +1,16 @@
 // add whatever parameters you deem necessary
-function pivotIndex() {
+function pivotIndex(arr) {
+  const totalSum = arr.reduce((sum, num) => sum + num, 0);
+  let leftSum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (leftSum === totalSum - leftSum - arr[i]) {
+      return i;
+    }
+    leftSum += arr[i];
+  }
+
+  return -1;
 }
+
+module.exports = pivotIndex;
